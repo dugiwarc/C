afbb6899e82f9b44c93177f4da6ab9f06e74f7e9
dugi <contact@tusk.world>
2019-05-08 03:46:09 +0200

    small fix

--- median.c
+++ median.c
@@ -2,26 +2,27 @@
 
 int main()
 {
-    float sum = 0, result; int input;
+    float sum = 0, result;
+    int input;
     float coeffs[8] = {0.3, 0.6, 0.6, 0.3, 0.4, 0.2, 0.3, 0.3};
     char subjects[8][10] = {"WEB", "ALGO", "ALGEBRA", "ENGLISH", "PF", "PPE", "BDD", "FCR"};
-    
+
     printf("\t\t\t\t==========================\n");
     printf("\t\t\t\tESTIMATION TABLE FOR SEM 2\n");
     printf("\t\t\t\t==========================\n");
 
-    for(int i = 0; i < 8; i++)
+    for (int i = 0; i < 8; i++)
     {
         printf("%s\t", subjects[i]);
         scanf("%d", &input);
-        sum+= coeffs[i] * input;
+        sum += coeffs[i] * input;
     }
-    result = ((sum / 3) + 12.6)/2;
+    result = ((sum / 3) + 12.64) / 2;
     printf("\t\t\t\t\tEstimate: %.2f\n", result);
 
     printf("\t\t\t\t==========================\n");
-    
-    if(result > 10)
+
+    if (result > 10)
         printf("\n\t\t\t\tCongrats, nothing to worry about\n");
 
     return 0;
